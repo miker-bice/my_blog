b880d98d19b21e01ab93ba3d99533ba8f4bacd0e
miker-bice <mikejerrson.galindez@gmail.com>
2022-03-23 14:31:42 +0800

    adding javascript mobile menu toggle successfull

--- static/js/base.js
+++ static/js/base.js
@@ -1,1 +1,10 @@
 console.log("connection successfull");
+
+//this is the target mobile menu
+const targetEl = document.getElementById('mobile-menu-4');
+
+const mobileMenuBtn = document.getElementById('mobile-menu-btn');
+
+mobileMenuBtn.addEventListener('click', () => {
+    targetEl.classList.toggle("hidden")
+});
