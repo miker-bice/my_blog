a321499e5f8db82a891eb72ab76b6a3467a84152
miker-bice <mikejerrson.galindez@gmail.com>
2022-04-03 11:00:57 +0800

    models registered to the admin site

--- blogApp/admin.py
+++ blogApp/admin.py
@@ -2,6 +2,14 @@ from django.contrib import admin
 from .models import Tag, Article
 
 
-# Register your models here.
 class TagAdmin(admin.ModelAdmin):
-    list_display = ('')
+    list_display = ['tag_name']
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'title', 'timestamp')
+
+
+# Register your models here.
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Article, ArticleAdmin)
